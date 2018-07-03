2652c69bc84aea67a7c3f3f4381481c2279c2c4a
Noura Al-Rashidi <nouraal-rashidi@Nouras-MacBook.local>
2018-07-03 18:37:37 +0300

    Completed task 3

--- restaurants/views.py
+++ restaurants/views.py
@@ -7,6 +7,26 @@ def restaurant_list(request):
 
     context = {
 
+    	"my_list": [
+
+    {
+    	"restaurant_name": "KFC",
+    	"food_type": "fried chicken",
+    },
+
+    {
+    	"restaurant_name": "pick",
+    	"food_type": "frozen yoghurt",
+
+    },
+
+    {
+    	"restaurant_name": "cucou",
+    	"food_type": "fries"
+    },
+
+    ],
+
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +34,14 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+
+    "my_object": 
+
+    {
+    	"restaurant_name": "KFC",
+    	"food_type": "fried chicken",
+    },
+
 
     }
     return render(request, 'detail.html', context)
